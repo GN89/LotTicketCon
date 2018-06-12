306d0f01f127aeb016c70e445cfd238920eb7d8f
German Nemcov <germannemcov@outlook.com>
2018-06-12 17:20:13 +0700

    DevStart

---
+++ GN89.Lottery/IMatchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GN89.Lottery
+{
+    public interface IMatchable
+    {
+        bool IsMatch { get; }
+    }
+}
